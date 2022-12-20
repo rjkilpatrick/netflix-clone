92e2d429893a8bbd6dd25973f67a94db296a6a4e
John Kilpatrick <john.kilpatrick@outlook.com>
2022-12-20 10:53:23 +0000

    feat: fix css

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,47 @@
 import React from "react";
 import "./App.css";
 
+const Header = () => {
+  return (
+    <header>
+      <img id="logo" src="/assets/logo.png" alt="Netflix" width="300px" height="150px" />
+      <nav className="navbar">
+        <a href="#home" class="active">
+          Home
+        </a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Footer</p>
+    </footer>
+  );
+};
+
 const App = () => {
   return (
     <div className="home">
-      <header>
-        <img src="/assets/logo.png" alt="Netflix" />
-        {/* <nav>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">TV Shows</a>
-            </li>
-            <li>
-              <a href="#">Movies</a>
-            </li>
-            <li>
-              <a href="#">My List</a>
-            </li>
-          </ul>
-        </nav> */}
-        <div class="navbar">
-          <a href="#home" class="active">
-            Home
-          </a>
-          <a href="#news">News</a>
-          <a href="#contact">Contact</a>
-        </div>
-      </header>
+      <Header />
       <main>
-        <h1>
-          Watch TV shows and movies anytime, anywhere — personalized for you.
-        </h1>
-        <button>Sign Up Free</button>
+        <div className="card" id="sign-up">
+          <h1 id="hero-text">
+            Watch TV shows and movies anytime, anywhere — personalized for you.
+          </h1>
+          <a href="#sign-up">
+            <button>Sign Up Free</button>
+          </a>
+          <video id="background-video" autoPlay loop muted>
+            <source src="C:\Users\rjk217\OneDrive - University of Exeter\Videos\2022-12-02 15-13-37.mkv"/>
+          </video>
+        </div>
       </main>
+      <Footer />
     </div>
   );
 };
