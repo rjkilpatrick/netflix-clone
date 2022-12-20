43ece87161b7255af545d54315ce0988b8a98326
John Kilpatrick <john.kilpatrick@outlook.com>
2022-12-20 14:22:36 +0000

    feat: Better header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,22 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <img
-        id="logo"
-        src="/assets/logo.png"
-        alt="Netflix"
-        width="300px"
-        height="150px"
-      />
-      <nav className="navbar">
-        <a href="#home" className="active">
-          Home
-        </a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-      </nav>
+      <div className="header clip">
+        <img
+          id="logo"
+          src="/assets/logo.png"
+          alt="Netflix"
+          width="300px"
+          height="150px"
+        />
+        <nav className="navbar">
+          <a href="#home" className="active">
+            Home
+          </a>
+          <a href="#news">News</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </div>
     </header>
   );
 };
